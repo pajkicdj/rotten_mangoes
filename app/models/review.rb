e75abee464024adce69e53de77066d93c47d95d6
DJINA <pajkic.djina@gmail.com>
2016-03-01 15:31:19 +0000

    added V1

---
+++ app/models/review.rb
@@ -0,0 +1,23 @@
+class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :movie
+  validates :user, :movie, :text,
+    presence: true
+
+  # validates :movie,
+  #   presence: true
+
+  # validates :text,
+  #   presence: true
+
+  validates :rating_out_of_ten,
+    numericality: { only_integer: true, 
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 10 }
+  # validates :rating_out_of_ten,
+  #   numericality: { greater_than_or_equal_to: 1 }
+  # validates :rating_out_of_ten,
+  #   numericality: { less_than_or_equal_to: 10 }
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '68d258e8d30844a35f18282541716f1ace141b8365f723a459247c05d3cf5d5ac32e6e4fe6931b011a11b20ae1678d78a0fef99e242d2afc01b86aab7350c80d'
